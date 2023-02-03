01d63ac4124fc2f0739d14997678383bd53433c1
alonso-estevam <alonso_estevam@outlook.com>
2023-02-03 09:34:33 -0300

    ajustes na ordem de execução dos comandos sql

--- desafio_1.sql
+++ desafio_1.sql
@@ -1,23 +1,26 @@
-CREATE TABLE "tb_produtos" (
-  "codigo" serial PRIMARY KEY,
-  "nome" varchar(100) NOT NULL,
-  "preco_unitario" numeric(6,2),
-  "categoria" int NOT NULL REFERENCES "tb_categorias" ("id")
+CREATE TABLE tb_categorias (
+  id serial PRIMARY KEY,
+  nome varchar(80) NOT NULL
 );
 
-CREATE TABLE "tb_categorias" (
-  "id" serial PRIMARY KEY,
-  "nome" varchar(80) NOT NULL
+
+CREATE TABLE tb_produtos (
+  codigo serial PRIMARY KEY,
+  nome varchar(100) NOT NULL,
+  valor_unitario numeric(6,2),
+  categoria int NOT NULL REFERENCES tb_categorias (id)
 );
+
 
 INSERT INTO tb_categorias (nome)
 VALUES	('outros'),
 	('eletrodomesticos'),
 	('eletronicos'),
 	('papelaria'),
-	('livros')
+	('livros');
 
-INSERT INTO tb_produtos (nome, preco_unitario, categoria)
+
+INSERT INTO tb_produtos (nome, valor_unitario, categoria)
 VALUES	('Livro "Head First: Java"', 74.89, 5),
 	('Livro "Projeto de Banco de Dados"', 59.49, 5),
 	('Ventilador Arno', 189.90, 2),
@@ -27,9 +30,11 @@ VALUES	('Livro "Head First: Java"', 74.89, 5),
 	('Fone de Ouvido Headset com Microfone', 84.10, 3),
 	('Conjunto Pincel Marcador de Texto', 13.90, 4),
 	('Bloco Adesivo Smart Notes', 21.42, 4),
-	('Papel Sulfite, Chamex, A4, 75g', 19.80, 4),
+	('Papel Sulfite Chamex A4, 75g', 19.80, 4),
 	('Caneta Esferográfica Colorida', 4.50, 4);
 
-UPDATE tb_produtos SET preco_unitario = preco_unitario + (preco_unitario * 0.1) WHERE categoria <> 4;
 
-UPDATE tb_produtos SET preco_unitario = preco_unitario + (preco_unitario * 0.15) WHERE categoria = 4;
+UPDATE tb_produtos SET valor_unitario = valor_unitario + (valor_unitario * 0.1) WHERE categoria <> 4;
+
+
+UPDATE tb_produtos SET valor_unitario = valor_unitario + (valor_unitario * 0.15) WHERE categoria = 4;
